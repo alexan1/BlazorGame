e5a212dc835fd531df3688aa8ef45ac72b215f82
Alexandre Konioukhov <alexan1@gmail.com>
2019-12-14 23:13:29 -0500

    search

---
+++ Pages/PeopleBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Net.Http.HttpClient;
+//using static System.Net.WebRequestMethods;
+using Microsoft.AspNetCore.Blazor;
+
+namespace BlazorGame.Pages 
+{
+    public class PeopleBase : ComponentBase
+    {
+        private Person[] people;
+
+        protected override async Task OnInitializedAsync()
+        {
+            //people = await Http.GetJsonAsync<Person[]>("sample-data/people.json");
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public int Rating { get; set; }
+        }
+    }
+}
