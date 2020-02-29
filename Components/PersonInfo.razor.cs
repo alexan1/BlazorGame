eabb17bac2197a21c050c5c1c41d99f9e7c8b040
Alexandre Konioukhov <alexan1@gmail.com>
2020-02-28 23:50:21 -0500

    rating

--- Components/PersonInfo.razor.cs
+++ Components/PersonInfo.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorGame.Pages;
+using Microsoft.AspNetCore.Components;
 using WikiDataLib;
 
 namespace BlazorGame.Components
@@ -7,5 +8,12 @@ namespace BlazorGame.Components
     {
         [Parameter]
         public PersonR Person { get; set; }
+
+        public void Rate(int value)
+        {
+            var userID = "anonymous";
+            var rating = new Rating();// { PersonId = 1;  }
+            var linkbase = "https://people3api.azurewebsites.net/api/Ratings/";
+        }
     }
 }
